08c8e386b16335df225d378c513dfd04dcb53ec8
박초린 <151991451+Parkchorin666@users.noreply.github.com>
2023-12-02 19:28:49 +0900

    Update start.py

--- start.py
+++ start.py
@@ -1,35 +1,10 @@
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from PIL import Image, ImageDraw, ImageFont
+from moviepy.editor import TextClip
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+def generate_video(video_content):
+    # 동영상 생성 로직을 구현하세요. 예시로 텍스트를 포함한 동영상 생성
+    clip = TextClip(video_content, fontsize=70, color='white', bg_color='black')
+    clip.write_videofile("generated_video.mp4", codec="libx264")
 
-class VideoForm(FlaskForm):
-    content = StringField('Content')
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = VideoForm()
-
-    if form.validate_on_submit():
-        content = form.content.data
-      
-def create_video(content):
-    image = Image.new('RGB', (500, 500), color='white')
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.load_default()
-
-    draw.text((10, 10), content, font=font, fill='black')
-
-    image.save('output_video.gif', save_all=True, duration=1000, loop=0)
-
-# 위의 코드에서 동영상을 생성하는 부분에 아래 코드를 추가합니다.
-# content 변수는 폼에서 입력한 내용입니다.
-# create_video(content)
-
-    return render_template('index.html', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# 예시: 폼에서 받은 내용을 사용하여 동영상 생성
+form_input = "GitHub Pages에서 동영상 생성 테스트 중"
+generate_video(form_input)
